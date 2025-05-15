f7ca9bf90ba536ae1ba39022ded96aa3182c6203
victorviegasgomes <vviegasgomes@gmail.com>
2025-05-15 23:04:13 +0100

    repo actualizado 15/05/2025

---
+++ PRATICA/pratica19/js/index.js
@@ -0,0 +1,9 @@
+let resultado = document.querySelector("#resultado")
+let num = 0
+for(let i = 0; i < 5; i++) {
+  for(let i = 0; i < 5; i++) {
+    num += 1
+    document.write(`${num}\n`)
+  }
+  document.write("\n\n")
+}
